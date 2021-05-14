7165b72f2481957cb97fdf0fc16a2e4974bc2e83
btprojectt <80315062+btprojectt@users.noreply.github.com>
2021-05-14 15:11:43 +0800

    Add files via upload

---
+++ museum/urlTest/urlTest.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<view>
+			<button @click="loginin">点击</button>
+		</view>
+		<!-- <view v-for="ite in museum" :key="ite.pk">
+			{{ite.fields.collectionintroduction}}
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				// museum: []
+			}
+		},
+		methods: {
+			async loginin() {
+				const res = await uni.request({	
+					url: 'http://127.0.0.1:8000/userstest',
+					method: 'POST',
+					data: {
+						username: 'userdemo',
+						password: 'userdemo'
+					},
+					header: {
+						'content-type': 'application/x-www-form-urlencoded', 
+					},
+					success: () => {
+						console.log('succeed')
+					}
+				})
+				console.log(res)
+			}
+		},
+		onLoad() {
+			
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
